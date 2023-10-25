400d7dbca300431e2582e3a0020409fe289c836b
Steven Chang <steven.chang@latticeworkinc.com>
2023-10-25 09:32:06 +0800

    feat(vm): add pwd

--- lib/bloc/virtual_amber_bloc.dart
+++ lib/bloc/virtual_amber_bloc.dart
@@ -4,26 +4,42 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:process_run/shell.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum Error { noPassword, none }
 
 class VirtualAmberBloc {
-  var shell = Shell(
-    runInShell: true,
-  );
-  var password = "lL10127142";
+  var shell = Shell();
+  var _password = "";
   PublishSubject<bool> _isConnectServerSubject = PublishSubject<bool>();
   Stream<bool> get isConnectServerStream => _isConnectServerSubject.stream;
   Future<String> getPath() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    return directory.path + "/virtualAmberLink.command";
+    return  directory.path + "/virtualAmberLink.command";
   }
 
-  Future<void> connectServer() async {
+  Future<void> setPwd(String pwd) async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _password = pwd;
+    await prefs.setString('pwd', _password);
+  }
+
+  Future<Error> connectServer() async {
     var path = await getPath();
     final File file = File(path);
-    await file.writeAsString("sshpass -p lL10127142 ssh earth");
-    await shell.run('''
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? pwd = prefs.getString('pwd');
+    if (pwd != null) {
+      _password = pwd;
+      await file.writeAsString("sshpass -p ${_password} ssh earth");
+      await shell.run('''
         chmod +x ${path}
         open ${path}
       ''').then((value) {});
+      return Error.none;
+    } else {
+      return Error.noPassword;
+    }
   }
 }
--- lib/page/virtual_amber_page.dart
+++ lib/page/virtual_amber_page.dart
@@ -10,12 +10,49 @@ class VirtualAmberPage extends StatefulWidget {
 
 class _VirtualAmberPageState extends State<VirtualAmberPage> {
   var bloc = VirtualAmberBloc();
+
+  Future<void> connectServer() async {
+    var err = await bloc.connectServer();
+    if (err == Error.noPassword) {
+      showDialog(
+        context: context,
+        builder: (context) => InputPasswordDialog(bloc),
+      ).then((value) {
+        connectServer();
+      });
+    } else {
+      Clipboard.setData(ClipboardData(text: "lw-virt"));
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Hint'),
+              content: const Text("Copy 'lw-virt' to clipboard."),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          ToolBar(),
+          Container(
+            color: Colors.red.withOpacity(0.1),
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: ToolBar(),
+            ),
+          ),
         ],
       ),
     );
@@ -34,26 +71,63 @@ class _VirtualAmberPageState extends State<VirtualAmberPage> {
                 backgroundColor: Colors.grey,
                 foregroundColor: Colors.white),
             onPressed: () {
-              bloc.connectServer();
-              Clipboard.setData(ClipboardData(text: "lw-virt"));
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                    title: const Text('Hint'),
-                    content: const Text("Copy 'lw-virt' to clipboard."),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('OK'),
-                      ),
-                    ],
-                  );
-                  });
+              connectServer();
             },
             label: Text("Connecting Virtual Server")),
+        TextButton.icon(
+            icon: Icon(Icons.password),
+            style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                backgroundColor: Colors.purple,
+                foregroundColor: Colors.white),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => InputPasswordDialog(bloc),
+              );
+            },
+            label: Text("reset password")),
+      ],
+    );
+  }
+}
+
+class InputPasswordDialog extends StatefulWidget {
+  InputPasswordDialog(this.bloc);
+  final VirtualAmberBloc bloc;
+  @override
+  _InputPasswordDialogState createState() => _InputPasswordDialogState();
+}
+
+class _InputPasswordDialogState extends State<InputPasswordDialog> {
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter Password'),
+      content: TextField(
+        controller: _passwordController,
+        obscureText: true,
+        decoration: InputDecoration(hintText: 'Password'),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            // TODO: Validate the password and do something with it.
+            widget.bloc.setPwd(_passwordController.text.toString());
+            Navigator.of(context).pop();
+          },
+          child: Text('Submit'),
+        ),
       ],
     );
   }
